ecead94c8b5777068d7407401b911c1e9a7ec675
Piyush-Arora03 <piyush.2023ug1113@iiitranchi.ac.in>
2026-04-04 17:10:33 +0530

    using hilt to provide singleton database instance

---
+++ app/src/main/java/com/example/financeapp/data/AppModule.kt
@@ -0,0 +1,40 @@
+package com.example.financeapp.data
+
+import android.content.Context
+import androidx.room.Room
+import com.example.financeapp.data.local.FinanceDatabase
+import com.example.financeapp.data.local.GoalDao
+import com.example.financeapp.data.local.TransactionDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFinanceDatabase(@ApplicationContext applicationContext: Context): FinanceDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            FinanceDatabase::class.java,
+            "finance_companion_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTransactionDao(financeDatabase: FinanceDatabase): TransactionDao {
+        return financeDatabase.retrieveTransactionDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGoalDao(financeDatabase: FinanceDatabase): GoalDao {
+        return financeDatabase.retrieveGoalDao()
+    }
+}
