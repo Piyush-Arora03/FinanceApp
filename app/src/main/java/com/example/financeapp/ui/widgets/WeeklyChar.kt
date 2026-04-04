386b6d40cd17920add032b27ac7e913e759bd632
Piyush-Arora03 <piyush.2023ug1113@iiitranchi.ac.in>
2026-04-04 17:24:04 +0530

    fixed error in weekly chart

--- app/src/main/java/com/example/financeapp/ui/widgets/WeeklyChar.kt
+++ app/src/main/java/com/example/financeapp/ui/widgets/WeeklyChar.kt
@@ -1,5 +1,4 @@
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -7,12 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.financeapp.data.models.DailySpendingSummary
@@ -78,7 +75,7 @@ fun WeeklySpendingChartCard(
                     ) {
                         Canvas(
                             modifier = Modifier
-                                .fillMaxWidth(0.6f
+                                .fillMaxWidth(0.6f)
                                 .fillMaxHeight(barHeightPercentage)
                         ) {
                             drawRoundRect(
